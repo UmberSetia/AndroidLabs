6001678ef7d77b100cfdd7505865f613d9c8aa44
Umber Setia <umbersetia@gmail.com>
2018-11-05 17:38:31 -0800

    doInBackground started

--- app/src/main/java/com/example/umbersetia/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/umbersetia/androidlabs/WeatherForecast.java
@@ -1,8 +1,14 @@
 package com.example.umbersetia.androidlabs;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class WeatherForecast extends Activity {
     private ProgressBar progressBar;
@@ -13,5 +19,21 @@ public class WeatherForecast extends Activity {
         setContentView(R.layout.activity_weather_forecast);
 
         progressBar = findViewById(R.id.progressBar);
+    }
+
+    private class ForecastQuery extends AsyncTask<String,Integer,String> {
+        private String windSpeed, minTemp, maxTemp, currentTemp;
+        private Bitmap bitmap;
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
     }
 }
